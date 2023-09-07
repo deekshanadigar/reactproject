66a7ac77db94fa6caa79ea54ca294ab4d8c5a316
deekshanadigar <143542214+deekshanadigar@users.noreply.github.com>
2023-09-07 15:35:21 +0530

    React Projects

---
+++ restapi/restapi/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './tasklist.css';
+import TaskList from './tasklist';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
